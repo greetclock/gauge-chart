c2a1ea417fe33f348bb7d5b327b1b696d8983e60
Alexey Karpov <karpovad@yandex.ru>
2022-07-28 09:55:11 +0200

    chore: remove typedoc

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,12 +1,7 @@
 let webpackMerge = require('webpack-merge')
-let path = require('path')
 let commonConfig = require('./common.config.js')
-let TypedocWebpackPlugin = require('typedoc-webpack-plugin')
 let UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin')
 
 module.exports = webpackMerge(commonConfig(), {
-  plugins: [
-    new TypedocWebpackPlugin({}, path.resolve(__dirname, '../src')),
-    new UglifyJsPlugin({}),
-  ],
+  plugins: [new UglifyJsPlugin({})],
 })
