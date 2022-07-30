5a391df2066a8c4c6d1eec0682b662f20136f9ed
Alexey Karpov <karpovad@yandex.ru>
2022-07-30 10:43:09 +0200

    docs: added readme and changelog

--- old/webpack/helpers.js
+++
@@ -1,7 +0,0 @@
-let path = require('path')
-
-let ROOT = path.resolve(__dirname, '..')
-
-let root = path.join.bind(path, ROOT)
-
-exports.root = root
--- old/webpack/prod.config.js
+++
@@ -1,7 +0,0 @@
-let webpackMerge = require('webpack-merge')
-let commonConfig = require('./common.config.js')
-let UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
-module.exports = webpackMerge(commonConfig(), {
-  plugins: [new UglifyJsPlugin({})],
-})
